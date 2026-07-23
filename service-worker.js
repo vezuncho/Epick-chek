1aa533609e4432dcbf6b57827080ac5fa752803f
vezuncho <vezunchosr@gmail.com>
2026-07-23 13:28:38 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='epi-check-edit-scroll-20260723-v1';
+const CACHE_VERSION='epi-check-aceptar-cabecera-20260723-v1';
 const APP_SHELL=['./manifest.json','./icon-192.png','./icon-512.png','./js/17-supabase-photos.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_VERSION).then(async c=>{for(const u of APP_SHELL){try{await c.add(u)}catch(_){}}}));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_VERSION).map(k=>caches.delete(k)))));self.clients.claim()});
